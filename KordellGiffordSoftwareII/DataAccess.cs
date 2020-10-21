eca17c33ead1d30d64de1edc9d88f5ed28e3e703
Kordell Gifford <kgiff14@student.wgu.edu>
2020-10-21 08:30:22 -0600

    Add project files.

---
+++ KordellGiffordSoftwareII/DataAccess.cs
@@ -0,0 +1,115 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KordellGiffordSoftwareII
+{
+    class DataAccess
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public DataAccess()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            server = "wgudb.ucertify.com,3306";
+            database = "U07wQh";
+            uid = "U07wQh";
+            password = "53689154201";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public void Insert(string query)
+        {
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        public DataTable Select(string query)
+        {
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                DataTable dt = new DataTable();
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataAdapter ad = new MySqlDataAdapter(selectCommand: cmd);
+                ad.Fill(dt);
+                //MySqlDataReader dataReader = cmd.ExecuteReader();
+                //while (dataReader.Read())
+                //{
+
+                //}
+                ////close Data Reader
+                //dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return dt;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ KordellGiffordSoftwareII/Login.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KordellGiffordSoftwareII
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        DataAccess da = new DataAccess();
+        DataTable dt = new DataTable();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             dataGridView1.DataSource = da.Select("SELECT * FROM user");
+        }
+    }
+}
