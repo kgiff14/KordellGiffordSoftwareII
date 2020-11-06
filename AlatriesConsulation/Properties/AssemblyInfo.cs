f6f903a203781f300d62ccb9537b9bfd9e48a8bb
kordellgifford <kordellgifford@conservice.com>
2020-11-06 13:27:29 -0700

    added unit test

---
+++ AlatriesConsulation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AlatriesConsulation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Service")]
+[assembly: AssemblyProduct("AlatriesConsulation")]
+[assembly: AssemblyCopyright("Copyright © Service 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a28b8079-9cf8-4176-95b5-bbab0c51e11e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AlatriesConsulation/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KordellGiffordCapstone;
+
+namespace AlatriesConsulation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        DataAccess da = new DataAccess();
+
+        [TestMethod]
+        public void ModifyCustomerLookupTest()
+        {
+            var name = "";
+            da.OpenConnection();
+            
+            da.CloseConnection();
+
+            Assert.IsTrue();
+        }
+    }
+}
